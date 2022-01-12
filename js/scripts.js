9674dbc59d2c17e142643c1772bf27d2a4ba625f
Eric Crudup <briefcasemanx@gmail.com>
2022-01-12 13:16:37 -0800

    added form that puts text into a paragraph
    Co-Authored by: Tony Martinez<tony.martinez5897@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const nameInput = $("input#person").val();
 
+    $(".person").text(nameInput);
+
+    $("#letter-text").show();
+  });
 
 
 });
